053de8f0abc18d96b165e7c3cb8dc8a8d6ea1dad
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-04-16 16:34:39 +0200

    Corretto refuso API

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ const API_KEY = '17411c00243f4aba8f358b714d83efbc';
 const movieAPI = {
   async searchMovies(query) {
     try {
-      const response = await axios.get('17411c00243f4aba8f358b714d83efbc', {
+      const response = await axios.get('`https://api.themoviedb.org/3/search/movie`,', {
         params: {
           api_key: API_KEY,
           query: query,
