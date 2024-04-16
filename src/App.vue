a306190a04f2667d7251a9459428cf7bcb8c02e1
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-04-16 16:33:49 +0200

    Migliorato criterio ricerca v-if. Bug-fix: result.title funziona. Importato API in script.

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,11 @@
   <div>
     <input type="text" v-model="searchQuery" @keyup.enter="search" placeholder="Ricerca un film o una serie TV">
     <button @click="search">Cerca film</button>
-    <div v-if="searchResults.length > 0">
+    <div v-if="searchResults && searchResults.length > 0 && searchResults !== null">
       <h2>Risultati della ricerca per "{{ searchQuery }}"</h2>
       <ul>
         <li v-for="result in searchResults" :key="result.id">
-          {{ result.searchQuery }}
+          {{ result.title }}
         </li>
       </ul>
     </div>
@@ -14,22 +14,33 @@
 </template>
 
 <script>
-import movieAPI from './api'; 
+import axios from 'axios';
+// import ApiService from './api';
 
 export default {
   data() {
     return {
+      API_KEY: '17411c00243f4aba8f358b714d83efbc',
       searchQuery: '',
       searchResults: [],
     };
   },
 
   methods: {
-    async search() {
+    search() {
+      console.log("SEARCH")
       try {
-        const results = await ApiService.searchMovies(this.searchQuery);
+        axios.get('https://api.themoviedb.org/3/search/movie', {
+          params: {
+            api_key: this.API_KEY,
+            query: this.searchQuery,
+            language: 'it-IT',
+          },
+        }).then(res => {
+          this.searchResults = res.data.results;
+          console.log(res);
+        })
 
-        this.searchResults = results;
       } catch (error) {
         console.error('Si è verificato un errore durante la ricerca:', error);
       }
@@ -38,6 +49,4 @@ export default {
 };
 </script>
 
-<style>
-@use './style/general';
-</style>
+<style></style>
