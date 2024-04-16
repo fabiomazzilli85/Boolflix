ce64aef24cc46a798af71071fb1008579f8b98e5
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-04-16 13:06:45 +0200

    Rivisto il template in App.Vue. Importatoi api.

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,43 @@
-<script>
-import ApiService from './api';
-
-export default {
-  data() {
-  return {
-    searchQuery: '',
-    searchResults: [],
-  };
-},
-
-  methods: {
-    async search() {
-      try {
-        await ApiService.searchMovies(this.searchQuery);
-      } catch (error) {
-      }
-    },
-  },
-};
-</script>
-
 <template>
-   <div>
+  <div>
     <input type="text" v-model="searchQuery" @keyup.enter="search" placeholder="Ricerca un film o una serie TV">
-    <button @click="search">Cerca</button>
+    <button @click="search">Cerca film</button>
     <div v-if="searchResults.length > 0">
       <h2>Risultati della ricerca per "{{ searchQuery }}"</h2>
       <ul>
         <li v-for="result in searchResults" :key="result.id">
-          {{ result.original_title }}
+          {{ result.searchQuery }}
         </li>
       </ul>
     </div>
   </div>
 </template>
 
+<script>
+import ApiService from './api';
+
+export default {
+  data() {
+    return {
+      searchQuery: '',
+      searchResults: [],
+    };
+  },
+
+  methods: {
+    async search() {
+      try {
+        const results = await ApiService.searchMovies(this.searchQuery);
+
+        this.searchResults = results;
+      } catch (error) {
+        console.error('Si è verificato un errore durante la ricerca:', error);
+      }
+    },
+  },
+};
+</script>
+
 <style>
 </style>
+
--- src/api.js
+++ src/api.js
@@ -1,29 +1,23 @@
 import axios from 'axios';
 
-const API_KEY = 'e99307154c6dfb0b4750f6603256716d';
-
-export default {
+const API_KEY = '17411c00243f4aba8f358b714d83efbc'; 
+const movieAPI = {
   async searchMovies(query) {
     try {
-    //   const response1 = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}`);
-    //   console.log(response1.data);
-    //   console.log(response1.data.results);
-    // Questa è una possibilità che Gianluca ci ha prospettato, evidenziando tutti i limiti del caso.
-    // Gianluca ci ha consigliato l'approccio seguente, che è più chiaro da interpretare e da leggere.
-
-
-      const response = axios.get('https://api.themoviedb.org/3/search/movie', {
+      const response = await axios.get('17411c00243f4aba8f358b714d83efbc', {
         params: {
           api_key: API_KEY,
           query: query,
           language: 'it-IT',
         },
       });
-      console.log(response.data);
-      console.log(response.data.results);
+      console.log(response.data.results); 
+      return response.data.results; 
     } catch (error) {
       console.error('Errore durante la ricerca dei film:', error);
       throw error;
     }
   },
 };
+
+export default movieAPI;
