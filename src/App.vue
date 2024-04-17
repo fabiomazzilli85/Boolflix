e9e5c2c225e3c960c7c94db42e09c2e2a6efbe2e
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-04-17 15:36:37 +0200

    Aggiunto e registrato componente Serie.Vue

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <search />
   <results />
   <card />
+  <serie />
 </template>
 
 <script>
 import Search from './components/search.vue';
 import Results from './components/results.vue';
 import Card from './components/card.vue';
+import Serie from './components/serie.vue';
 
 export default {
   components: {
     Search,
     Results,
     Card,
+    Serie,
   }
 }
 </script>
