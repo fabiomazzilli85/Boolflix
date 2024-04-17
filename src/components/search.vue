b5f4bcd21c96ef8241d203929486ed0bd7d59892
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-04-17 13:43:59 +0200

    Bug-fix: in v-model "searchQuery" e non "query".

--- src/components/search.vue
+++ src/components/search.vue
@@ -12,28 +12,26 @@ import { store } from '../store.js'
 export default {
     data() {
         return {
-            API_KEY: '17411c00243f4aba8f358b714d83efbc',
             searchQuery: '',
-            searchResults: [],
         };
     },
 
     methods: {
         search() {
-        axios.get('https://api.themoviedb.org/3/search/movie',{
-          params: {
-            api_key: 'e99307154c6dfb0b4750f6603256716d',
-            query: this.query
-          }
-        }).then((res) => {
-          console.log(res.data.results)
-          store.movies = res.data.results
-        })
+            axios.get('https://api.themoviedb.org/3/search/movie', {
+                params: {
+                    api_key: 'e99307154c6dfb0b4750f6603256716d',
+                    query: this.searchQuery
+                }
+            }).then((res) => {
+                console.log(res.data.results)
+                store.movies = res.data.results
+            })
 
-        console.log('recupero i dati di serie tv')
-      }
+            console.log('recupero i dati di serie tv')
+        }
     },
-  }
+}
 </script>
 
 <style lang="scss" scoped>
