00fc498a4f46b4edf651c47b6075df70a17b0369
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-04-17 16:40:41 +0200

    Da template rimossa card e serie. In script rimossa serie.

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,23 @@
 <template>
   <search />
   <results />
-  <card />
-  <serie />
 </template>
 
 <script>
 import Search from './components/search.vue';
 import Results from './components/results.vue';
 import Card from './components/card.vue';
-import Serie from './components/serie.vue';
+
 
 export default {
   components: {
     Search,
     Results,
     Card,
-    Serie,
+  },
+  data() {
+    return {
+    }
   }
 }
 </script>
