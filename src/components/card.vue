363ede2c0f1d0704f6bda12d4d3bef8b5d44ab19
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-04-17 14:09:14 +0200

    In template stampato titolo film, lingua e durata.

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,9 +1,21 @@
 <template>
-
+    <ul>
+        <li><strong>Titolo originale del film</strong>: {{ item.original_title }}</li>
+        <li><strong>Lingua</strong>: {{ item.original_language }}</li>
+        <li><strong>Voto della critica</strong>: {{ item.vote_average }}</li>
+    </ul>
 </template>
 
 <script>
+export default {
+    props: {
+        item: {
+            type: Object,
+            required: true
+        }
+    }
+}
 </script>
 
-<style>
+<style lang="scss" scoped>
 </style>
