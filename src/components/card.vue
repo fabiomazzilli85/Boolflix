5c6af1b8295371490e201dc5c15e81feea1b3136
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-04-17 15:37:08 +0200

    In card inserita formattazione ul li, tolto list style

--- src/components/card.vue
+++ src/components/card.vue
@@ -3,6 +3,7 @@
         <li><strong>Titolo originale</strong>: {{ item.original_title }}</li>
         <li><strong>Lingua</strong>: {{ item.original_language }}</li>
         <li><strong>Voto della critica</strong>: {{ item.vote_average }}</li>
+        <li><strong>Uscita nei cinema</strong>: {{ item.release_date }}</li>
     </ul>
 </template>
 
@@ -18,4 +19,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+ul,
+li {
+    list-style: none;
+}
 </style>
