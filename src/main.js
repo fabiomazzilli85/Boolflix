6a13cd5c932baca81a3f206c2ca8c7801e83a644
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-04-18 09:00:02 +0200

    In Main.js importo icone e svg core di Font Awesome (ripreso da Gianluca).

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
 import { createApp } from 'vue'
-// import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+// Qui inizia il lavoro di importazione da FontAwesome.
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import {
+	faStar,
+	// faStarOfLife,
+	// faPlay,
+	// faUser,
+} from '@fortawesome/free-solid-svg-icons'
+import { faStar as faStarReg } from '@fortawesome/free-regular-svg-icons'
+
+library.add(faStar, faStarReg)
+
+const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.mount('#app')
