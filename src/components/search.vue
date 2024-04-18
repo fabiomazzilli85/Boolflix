f373835f19c1d1751370f71d91b61a8f78f0df86
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-04-18 16:31:12 +0200

    language: 'it_IT' aggiunto a params

--- src/components/search.vue
+++ src/components/search.vue
@@ -30,6 +30,7 @@ export default {
             axios.get('https://api.themoviedb.org/3/search/movie', {
                 params: {
                     api_key: 'e99307154c6dfb0b4750f6603256716d',
+                    language: 'it_IT',
                     query: this.searchQuery
                 }
             })
