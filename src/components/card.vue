449fca82289052cd508ec1d32dc1220af6afe72d
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-04-18 09:55:04 +0200

    Inserite bandiere in assets

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,19 +1,32 @@
 <template>
-   <ul>
+    <ul>
         <li><strong>Titolo originale</strong>: {{ item.original_title }}</li>
-        <li><strong>Lingua</strong>: {{ item.original_language }}</li>
+        <img class="flag" v-if="item.original_language === 'it'" :src="itFlag" alt="Italian Flag">
+        <img class="flag" v-if="item.original_language === 'fr'" :src="frFlag" alt="French Flag">
+        <img class="flag" v-if="item.original_language === 'en'" :src="enFlag" alt="English Flag">
         <li><strong>Voto della critica</strong>: {{ item.vote_average }}</li>
         <li><strong>Uscita nei cinema</strong>: {{ item.release_date }}</li>
     </ul>
 </template>
 
 <script>
+import itFlag from '../assets/it.png';
+import frFlag from '../assets/fr.png';
+import enFlag from '../assets/en.jpg';
+
 export default {
     props: {
         item: {
             type: Object,
             required: true
         }
+    },
+    data() {
+        return {
+            itFlag: itFlag,
+            frFlag: frFlag, 
+            enFlag: enFlag,
+        };
     }
 }
 </script>
@@ -23,5 +36,9 @@ ul,
 li {
     list-style: none;
     color: black;
+}
+
+.flag {
+    width: 150px;
 }
 </style>
--- src/components/results.vue
+++ src/components/results.vue
@@ -13,7 +13,9 @@
     <ul v-if="store.tvs.length !== 0">
       <template v-for="tvShow in store.tvs" :key="tvShow.id">
         <li><strong>Nome</strong>: {{ tvShow.name }}</li>
-        <li><strong>Lingua</strong>: {{ tvShow.original_language }}</li>
+        <img class="flag" v-if="tvShow.original_language === 'it'" :src="itFlag" alt="Italian Flag">
+        <img class="flag" v-if="tvShow.original_language === 'fr'" :src="frFlag" alt="French Flag">
+        <img class="flag" v-if="tvShow.original_language === 'en'" :src="enFlag" alt="English Flag">
         <li><strong>Voto della critica</strong>: {{ tvShow.vote_average }}</li>
         <li><strong>Paese di Produzione</strong>: {{ tvShow.origin_country }}</li>
       </template>
@@ -27,20 +29,31 @@
 import { store } from '../store.js'
 import Card from './card.vue'
 
+import itFlag from '../assets/it.png';
+import frFlag from '../assets/fr.png';
+import enFlag from '../assets/en.jpg';
+
 export default {
   components: {
     Card
   },
   data() {
     return {
-      store: store
-    }
-  },
+        store: store,
+        itFlag: itFlag,
+        frFlag: frFlag, 
+        enFlag: enFlag,
+    };
+}
 }
 </script>
 
 <style lang="scss" scoped>
 @use './style/general';
+
+.flag {
+    width: 150px;
+}
 
 h2 {
   margin: 25px 0;
